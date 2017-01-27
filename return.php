537b81ac4ed8a7daf51cd79ba810ea9095ac5386
jessica <jesscasillas24@gmail.com>
2017-01-26 21:08:01 -0700

    Second part of form project

---
+++ return.php
@@ -0,0 +1,53 @@
+<?php
+//Connection to database
+include 'conn.php'
+//Select database
+
+$sql = "SELECT FROM form1";
+$result = $conn->query($sql);
+?>
+<!--Form starts here-->
+<!DOCTYPE HTML>
+<html>
+<body>
+<h1 align="center">Sign In-Log</h1>
+<table border ="1" align="center" style="line-height:25px;">
+<tr>
+<th>Name</th>
+<th>Meeting</th>
+<th>Time</th>
+<th>Badge</th>
+<th>Vehicle</th>
+</tr>
+
+
+<?php
+//Fetch data from DB
+if($result ->num_rows > 0){
+ while($row = $result->fetch_assoc()){
+?>
+
+<tr>
+<td><?php echo $row['Name']; ?></td>
+<td><?php echo $row['Meeting']; ?></td>
+<td><?php echo $row['Time']; ?></td>
+<td><?php echo $row['Badge']; ?></td>
+<td><?php echo $row['Vehicle']; ?></td>
+<!--Edit Option -->
+<td><a href="edit.php?edit_id=<?php echo $row['Name']; ?>" alt="edit">Edit</a></td>
+</tr>
+<?php
+ }
+}
+else
+{
+?>
+<tr>
+<th colspan-"2">No Data Found!</th>
+</tr>
+<?php
+}
+?>
+</body>
+</html>
+</table>
