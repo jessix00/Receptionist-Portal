9c1d4701b4bbb979c47f30e42c65511fceb1e342
jessica <jesscasillas24@gmail.com>
2017-02-09 21:01:27 -0700

    simple php connection im using to synch all docs

--- conn.php
+++ conn.php
@@ -9,6 +9,7 @@ $dbname = "form-test";
 $conn = new mysqli($server, $user, $pass, $dbname);
 //check connection 
 if($conn->connect_error){
-    die("error on connection:" .$connection->connecr_error);
+    die("error on connection:" .$connection->connect_error);
 }
+
 ?>
