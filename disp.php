62a8cb0820f10764b37e4578be47568b85348cfc
jessica <jesscasillas24@gmail.com>
2017-03-01 21:34:36 -0700

    Added styles to admin table - Added styles and new font.

--- disp.php
+++ disp.php
@@ -6,21 +6,61 @@ include 'conn.php';
 $sql = "SELECT * FROM form1 WHERE Display = '1'";
 $result = $conn->query($sql);
 ?>
-<!--Form starts here-->
-<!DOCTYPE HTML>
-<html>
-<body>
-<h1 align="center">Sign In-Log</h1>
-<table border ="1" align="center" style="line-height:25px;">
-<tr>
-<th>Name</th>
-<th>Company Name</th>
-<th>Meeting</th>
-<th>Time-Out </th>
-<th>Badge</th>
-<th>Vehicle</th>
-</tr>
 
+<!--Table starts here-->
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Client Sign-In Form</title>
+<!--Fonts/styles-->
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link href-"normalize.css" rel="stylesheet">
+	  <style type="text/css">
+            body {
+                font-family: "Roboto";
+                font-size: 20px;
+                background: #e2dedb;
+                color: #000;
+                }
+            h1 {
+                text-align: center;
+               }   
+
+               table {
+                    position: relative;
+                    margin: 20px auto 100px auto;
+                    border: solid 1px #b3aca7;
+               } 
+
+               th {
+                border: solid 1px #b3aca7;
+                padding: 5px;
+                background-color:#fff;
+               }
+               td {
+                   border: solid 1px #b3aca7;
+                   padding: 5px;
+                   background-color:#fff;
+               }
+               tr {
+                
+               }
+    </style>
+</head>
+<body>
+
+<h1>Sign-In Log</h1>
+  <!--  <table border ="1" align="center" style="line-height:25px;"> -->
+   <table>
+     <tr>
+            <th>Name</th>
+            <th>Company Name</th>
+            <th>Meeting</th>
+            <th>Time-Out </th>
+            <th>Badge</th>
+            <th>Vehicle</th>
+      </tr>
 
 <?php
 //Fetch data from DB
@@ -28,16 +68,16 @@ if($result ->num_rows > 0){
  while($row = $result->fetch_assoc()){
 ?>
 
-<tr>
-<td><?php echo $row['Name']; ?></td>
-<td><?php echo $row['CompanyName']; ?></td>
-<td><?php echo $row['Meeting']; ?></td>
-<td><?php echo $row['Timeout']; ?></td>
-<td><?php echo $row['Badge']; ?></td>
-<td><?php echo $row['Vehicle']; ?></td>
-<!--Edit Option -->
-<td><a href="edit.php?edit_id=<?php echo $row['ID']; ?>" alt="edit">Edit</a></td>
-</tr>
+    <tr>
+        <td><?php echo $row['Name']; ?></td>
+        <td><?php echo $row['CompanyName']; ?></td>
+        <td><?php echo $row['Meeting']; ?></td>
+        <td><?php echo $row['Timeout']; ?></td>
+        <td><?php echo $row['Badge']; ?></td>
+        <td><?php echo $row['Vehicle']; ?></td>
+        <!--Edit Option -->
+        <td><a href="edit.php?edit_id=<?php echo $row['ID']; ?>" alt="edit">Edit</a></td>
+    </tr>
 <?php
  }
 }
