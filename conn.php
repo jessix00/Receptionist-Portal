efac698d857b38b2f15db1ddc7725fadc0180a11
jessica <jesscasillas24@gmail.com>
2017-03-08 18:17:48 -0700

    minor changes

--- conn.php
+++ conn.php
@@ -3,9 +3,7 @@ $server = "localhost";
 $user = "root";
 $pass = "Hellokitty";
 $dbname = "form-test";
-
 //Create a connection in mysqli
-
 $conn = new mysqli($server, $user, $pass, $dbname);
 //check connection 
 if($conn->connect_error){
